c2916c18910a5e1ce65b89a18a0294ab044a65e0
nguyenyen2805 <DuyDQ.B20CN147@stu.ptit.edu.vn>
2024-11-06 17:40:17 +0700

    update path save avatar blog

--- module/blog/update.php
+++ module/blog/update.php
@@ -17,7 +17,7 @@ if (isset($_GET['blog_id'])) {
         if ($_FILES['image']['error'] !== UPLOAD_ERR_NO_FILE) {
             $image = $_FILES['image'];
             $extension = pathinfo($image['name'], PATHINFO_EXTENSION);
-            $target_dir = 'uploads/blog/';
+            $target_dir = 'uploads/blogs/';
             $new_avatar_path = $target_dir . uniqid() . '.' . $extension;
         }
 
