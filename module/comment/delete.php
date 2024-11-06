233047cd8444f210eed263447689581f8c8510e7
nguyenyen2805 <DuyDQ.B20CN147@stu.ptit.edu.vn>
2024-11-06 18:03:02 +0700

    fix SQLi in delete comment

--- module/comment/delete.php
+++ module/comment/delete.php
@@ -1,7 +1,7 @@
 <?php
 
-$comment_id = $_GET['comment_id'];
-$blog_id = $_GET['blog_id'];
+$comment_id = intval($_GET['comment_id']);
+$blog_id = intval($_GET['blog_id']);
 $query = "DELETE FROM comments WHERE comment_id = $comment_id";
 $result = $conn->query($query);
 if ($result == true) {
